74c03492904fc73f682c993741847b305b82fc5a
Hrant Virabyan <hvirabyan001@gmail.com>
2024-04-07 00:11:05 -0700

    udpate 4/7

---
+++ catalog.js
@@ -0,0 +1,9 @@
+document.getElementById("homeButton").onclick = function () {
+    location.href = "index2.html";
+    console.log("Clicked")
+};
+
+document.getElementById("sourceButton").onclick = function () {
+    location.href = "sources.html";
+    console.log("Clicked")
+};
